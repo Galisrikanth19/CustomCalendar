52b89f0e437d816cfd0e269d3c68732377933920
Gali Srikanth <galisrikanth19@gmail.com>
2024-04-19 19:21:02 +0530

    Initial Commit

---
+++ SwiftUIMock/SwiftUIMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMockApp.swift
+//  SwiftUIMock
+//
+//  Created by GaliSrikanth on 19/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
